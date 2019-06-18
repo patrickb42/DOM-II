3c9d9e1c1a71069859d36f590d278c1fdf57ee94
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-06-18 17:02:32 -0400

    four down

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// Your code goes here
+document.querySelector('.nav').addEventListener('click', (event) => {
+  if (event.target !== document.querySelector('.nav')) {
+    alert(`You clicked ${event.target.textContent}!
+Have a cookie... get it?`);
+  }
+});
+
+document.querySelectorAll('img')
+  .forEach((img) => {
+    img.addEventListener('mouseenter', (event) => {
+      const e = event;
+      e.target.style.opacity = '.75';
+    });
+    img.addEventListener('mouseleave', (event) => {
+      const e = event;
+      e.target.style.opacity = '1';
+    });
+  });
+
+let clipboard;
+document.querySelector('body')
+  .addEventListener('copy', () => {
+    navigator.clipboard.readText().then(data => alert(`You copied ${data}`));
+  });
